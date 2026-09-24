8c01f281835486b3c38d7a8fe0379723b792f80f
Arden <ardenpowl@gmail.com>
2026-09-24 16:44:40 -0700

    ok so this enumerates

--- include/USB_driver.h
+++ include/USB_driver.h
@@ -1,8 +1,147 @@
 #ifndef USB_driver_h
 #define USB_driver_h
 
-#include "avr/interrupt.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include "USB.h"
+#include "udDescriptors.h"
+
+#include <stdint.h>
+
+#define MAXEP 6
+
+typedef enum {
+    EPTYPE_CONTROL,
+    EPTYPE_ISOCHRONOUS,
+    EPTYPE_BULK,
+    EPTYPE_INTERRUPT
+} EPTYPE_ENUM;
+
+typedef enum {
+    EPDIR_OUT,
+    EPDIR_IN
+} EPDIR_ENUM;
+
+typedef enum {
+    EPBKTYPE_ONEBANK,
+    EPBKTYPE_DOUBLEBANK
+} EPBK_ENUM;
+
+typedef enum {
+    VBUS_NOPLUGDETECT,
+    VBUS_PLUGDETECT
+} VBUS_ENUM;
+
+typedef enum {
+    USBCLK_FREEZE,
+    USBCLK_UNFREEZE
+} USB_CLK_ENUM;
+
+typedef enum {
+    USB_DETACH,
+    USB_CONNECT
+} USB_DETACH_ENUM;
+
+typedef enum {
+    /* Enable an endpoint interrupt when an isochronous overflow or underflow condition is detected (OVERFI or UNDERFI). */
+    CFG_FLERRE = 128,
+    /* Enable an endpoint interrupt when the host sends an IN token but the endpoint cannot provide data, causing a NAK (NAKINI). */
+    CFG_NAKINE = 64,
+    /* Enable an endpoint interrupt when the host sends an OUT token but the endpoint cannot accept the received data, causing a NAK (NAKOUTI). */
+    CFG_NAKOUTE = 16,
+    /* Enable an endpoint interrupt after a SETUP packet is received. Normally used by control endpoint zero (RXSTPI). */
+    CFG_RXSTPE = 8,
+    /* Enable an endpoint interrupt after an OUT packet is received and its payload is available in the endpoint FIFO (RXOUTI). */
+    CFG_RXOUTE = 4,
+    /* Enable an endpoint interrupt after the endpoint returns a STALL handshake to the host (STALLEDI). */
+    CFG_STALLEDE = 2,
+    /* Enable an endpoint interrupt when the IN FIFO/bank is ready to accept the next packet from firmware (TXINI). */
+    CFG_TXINE = 1
+} UEIENX_CFG_ENUM;
+
+typedef enum {
+    PLL_STOP,
+    PLL_START,
+    PLL_CPUEXT
+} PLLCFG_ENUM;
+
+typedef enum {
+    USBFUNC_RETURN = 2,
+    USBFUNC_SETONE = 1,
+    USBFUNC_SETZERO = 0
+} USB_FUNCTION_RETURN_ENUM;
+
+typedef enum {
+    SM_SLEEPDISABLE,
+    SM_SLEEPENABLE,
+    SM_IDLE = (1 << 1),
+    SM_ADCNOISEREDUC = (2 << 1),
+    SM_POWERDOWN = (3 << 1),
+    SM_POWERSAVE = (4 << 1),
+    SM_STANDBY = (5 << 1),
+    SM_EXTENDSTANDBY = (6 << 1)
+} SM_ENUM;
 
 void USB_interface_init(void);
+//void USB_interface_powerOn(VBUS_ENUM VBUS_E);
+static void USB_interface_EPConfigure(uint8_t epNum, EPDIR_ENUM EPDIR_E, EPTYPE_ENUM EPTYPE_E, uint16_t epSizeB, EPBK_ENUM EPBK_E, UEIENX_CFG_ENUM CFG_E) {
+    epNum = epNum > MAXEP ? MAXEP : epNum;
+    UENUM = epNum;
+    while ((UENUM & 0x07) != epNum);
+
+    //USB_interface_EPReset(epNum);
+
+    if (epNum == 0) {
+        epSizeB = epSizeB > 64 ? 64 : epSizeB;
+    }
+    // only allowed to have one ep that is 256B
+
+    if (epSizeB <= 8) epSizeB = 0;
+    else if (epSizeB <= 16) epSizeB = 1;
+    else if (epSizeB <= 32) epSizeB = 2;
+    else if (epSizeB <= 64) epSizeB = 3;
+    else if (epSizeB <= 128) epSizeB = 4;
+    else epSizeB = 5;
+
+    UECONX |= (1 << EPEN);
+
+    UECFG0X = ((EPTYPE_E << EPTYPE0) | (EPDIR_E << EPDIR));
+    UECFG1X = ((epSizeB << EPSIZE0) | (EPBK_E << EPBK0) | (1 << ALLOC)); 
+
+    /*
+    if (!(UESTA0X & (1 << CFGOK))) {
+        cli();
+        PORTB &= ~(1 << PB0);
+        return;
+    }
+    */
+
+    while ((UESTA0X & (1 << CFGOK)) == 0) {
+        PORTB &= ~(1 << 0);
+    }
+    PORTB |= (1 << 0);
+
+    UERST = (1 << epNum);
+    UERST = 0;
+
+    UEIENX = CFG_E;
+}
+
+static uint8_t USB_interface_configStatus(USB_FUNCTION_RETURN_ENUM USBFUNC_E) {
+    static uint8_t status = 0;
+    switch (USBFUNC_E) {
+        case 0:
+            status = 0;
+            break;
+        case 1:
+            status = 1;
+            break;
+        case 2: 
+            break;
+    }
+    return status;
+}
+
+
 
 #endif // USB_driver_h
--- src/USB_driver.c
+++ src/USB_driver.c
@@ -28,3 +28,181 @@ void USB_interface_init(void) {
 
     sei();
 }
+
+ISR(USB_GEN_vect) {
+    uint8_t flags = UDINT;
+    UDINT = 0;
+    if (flags & UPRSMI) {
+        // resume usb...
+        /*
+        CPU_sleepMode(SM_SLEEPDISABLE);
+        PLL_configure(PLL_START);
+        USB_interface_clockManage(USBCLK_UNFREEZE);
+        */
+        // clear resume information?
+    }
+    if (flags & (1 << EORSTI)) {
+        USB_interface_EPConfigure(0, EPDIR_OUT, EPTYPE_CONTROL, 64, EPBKTYPE_ONEBANK, (CFG_RXSTPE));
+        USB_interface_configStatus(USBFUNC_SETZERO);
+        //PORTD &= ~(1 << 5);
+    }
+    if (flags & (1 << SOFI)) {
+        // sof...
+    }
+    if (flags & (1 << SUSPI)) {
+        // clear suspend bit?
+        /*
+        USB_interface_clockManage(USBCLK_FREEZE);
+        PLL_configure(PLL_STOP);
+        CPU_sleepMode(SM_POWERDOWN | SM_SLEEPENABLE);
+        */
+    }
+}
+
+ISR(USB_COM_vect) {
+    /*
+    When writing or reading a FIFO, since they are not memory mapped, so you do everything
+    through UEDATX (there is no descrirbed procedure for moving data). Basically, when you 
+    setup your endpoint, the ALLOC command allocates stuff required for your ep config., 
+    including your bank. When the USB controller receives a new packet, it fires the required 
+    interrupt, in which we begin to read (or write) to UEDATX. Any subsiquent read or write 
+    to UEDATX, increments an internal pointer to point UEDATX at the next byte in our DPRAM 
+    bank for processing. 
+    */
+    UENUM = 0;    
+    if (UEINTX & (1 << RXSTPI)) {
+        //UEINTX &= ~(1 << RXSTPI);
+        // Setup packet 
+        volatile uint8_t bmRequestType = UEDATX;
+        volatile uint8_t bRequest = UEDATX; // All USB defined traffic is encoded in little endian order.
+        volatile uint16_t wValue = UEDATX;  // Since little endian, LSB is the first byte, 
+        wValue |= UEDATX << 8;     // then we move MSB to the left by 8b to get our 2B per 'w.'
+        volatile uint16_t wIndex = UEDATX;
+        wIndex |= UEDATX << 8;
+        volatile uint16_t wLength = UEDATX;
+        wLength |= UEDATX << 8;
+
+        UEINTX &= ~((1 << RXSTPI) | (1 << RXOUTI) | (1 << TXINI)); // Clear UEINTX flags to handshake
+
+        PORTB ^= (1 << PB0);
+
+        // send device descriptor
+        if (bRequest == GET_DESCRIPTOR) { // bmRequestType == 128 && ...
+            // host sent GET_DESCRIPTOR, requesting only (wLength/8) bytes of the descriptor, 
+            // of type (wValue >> 8)
+            uint16_t descLen; // = sizeof(udDeviceDescriptor);
+            const uint8_t *descriptor;
+            uint16_t requestLen = wLength; // > 512 ? 512 : wLength;
+            uint16_t bytesSent = 0;
+            switch (wValue) {
+                case 0x0100: // getDescriptor(device)
+                    descriptor = udDeviceDescriptor;
+                    descLen = sizeof(udDeviceDescriptor);
+                    break;
+                case 0x0200: // getDescriptor(configuration)
+                    descriptor = udConfigurationDescriptor;
+                    descLen = sizeof(udConfigurationDescriptor);
+                    break;
+                case 0x0300: // getDescriptor(string, index 0) it wants the language (US)
+                    descriptor = udStringLanguageDescriptor;
+                    descLen = sizeof(udStringLanguageDescriptor);
+                    break;
+                case 0x0301: // getDescriptor(string, index 1) it wants the manufacturer String
+                    descriptor = udStringManufacturerDescriptor;
+                    descLen = sizeof(udStringManufacturerDescriptor);
+                    break;
+                case 0x0302: // getDescriptor(string, index 2) it wants the product String
+                    descriptor = udStringDeviceDescriptor;
+                    descLen = sizeof(udStringDeviceDescriptor);
+                    break;
+                case 0x0600: // getDescriptor(deviceQualifier)
+                    UECONX |= (1 << STALLRQ);
+                    break;
+                case 0x2200: // getDescriptor(HIDReport)
+                    descriptor = udHIDReportDescriptor;
+                    descLen = sizeof(udHIDReportDescriptor);
+                    break;
+                default: // Unknown bDescriptorType
+                    UECONX |= (1 << STALLRQ);
+                    cli();
+                    break;
+            }
+            descLen = requestLen > descLen ? descLen : requestLen;
+            while (descLen > 0) {
+                while(!(UEINTX & (1 << TXINI))); // wow
+                uint8_t packetLen = descLen > UD_EP0_SIZE ? UD_EP0_SIZE : descLen;
+                for (uint8_t i = 0; i < packetLen; i++) {
+                    UEDATX = pgm_read_byte(descriptor + bytesSent++);
+                }
+                descLen -= packetLen;
+                UEINTX &= ~(1 << TXINI);
+            }
+            return;
+        }
+
+        if (bRequest == SET_ADDRESS) {
+            UEINTX &= ~(1 << TXINI); //handshake
+            while(!(UEINTX & (1 << TXINI)));
+            UDADDR = wValue | (1 << ADDEN);
+            return;
+        }
+
+        if (bRequest == SET_CONFIGURATION) {
+            if (bmRequestType == 0 && USB_interface_configStatus(USBFUNC_RETURN) == 0) {
+                /*
+                Host sent a configuration request for endpoint wValue,
+                specified by the low byte in wValue.
+                bmRequestType: 0
+                bRequest: 9
+                wValue: 1
+                wIndex: 0
+                wLength: 0
+                */
+                UEINTX &= ~(1 << TXINI); //handshake
+               
+                // ep1
+                UENUM = 1; // = wValue
+                UECONX = (1 << EPEN);
+                UECFG0X = 0b11000001; // interrupt, in
+                UECFG1X = 0b00110010; // One Bank: 64B, Allocate
+                if (!(UESTA0X & (1 << CFGOK))) {
+                    cli();
+                    //_blink(100);
+                    return;
+                }
+                
+                // ep2
+                UENUM = 2;
+                UECONX = (1 << EPEN);
+                UECFG0X = 0b11000000; // interrupt, out
+                UECFG1X = 0b00110010; // One Bank: 64B, Allocate
+                if (!(UESTA0X & (1 << CFGOK))) {
+                    cli();
+                    //_blink(100);
+                    return;
+                }
+                
+                UERST = 0x7E; // 0b01111110
+                UERST = 0;
+                
+                return;
+            }
+        }
+        
+        if (bRequest == SET_IDLE) {
+            /* TODO: this
+            If there is no data change within the idle period,
+            resend the previous packet. If 0, only send data
+            when there is a change (no resends).
+            */
+            UEINTX &= ~(1 << TXINI);
+            return;
+        }
+
+        if (bRequest == GET_REPORT) {
+            return;
+        }   
+    }
+
+    UECONX |= (1 << STALLRQ);
+}
